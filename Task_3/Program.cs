c477f84216abcb986ad5c65d5f59ef66cc304fd5
MikhailYuzhakov <miha.yuzhakov@gmail.com>
2022-04-23 21:57:10 +0300

    Таблица кубов

---
+++ Task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+*/
+
+using static System.Math;
+
+Console.Clear();
+Console.WriteLine("Введите число N: ");
+int count = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= count; i++) {
+    Console.Write(Math.Pow(i, 3) + " ");
+}
+Console.WriteLine();
